1a96937eb0ae0726040b4245bb4fd80ba046100c
Daniel Keys <danieljkeys52@gmail.com>
2026-04-07 09:45:44 -0400

    Linter Changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,13 +7,31 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     path(route='register', view=views.registration, name='register'),
-    path(route='login', view=views.login_user, name='login'),  # API only
+    path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='get_dealer_reviews'),
-    path(route='get_dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='get_dealer_reviews'
+    ),
+    path(
+        route='get_dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
     path(route='add_review', view=views.add_review, name='add_review'),
-    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path(
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
+    path(
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
