9ef8b1e94c822c48923c50c6043f84750c81adc2
Daniel Keys <danieljkeys52@gmail.com>
2026-04-07 09:53:00 -0400

    Linter Changes

--- server/database/app.js
+++ server/database/app.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -19,10 +20,12 @@ const Dealerships = require('./dealership');
 // Initial data load
 try {
     Reviews.deleteMany({}).then(() => {
-        Reviews.insertMany(reviews_data['reviews']);
+        // Changed ['reviews'] to .reviews (Dot Notation)
+        Reviews.insertMany(reviews_data.reviews);
     });
     Dealerships.deleteMany({}).then(() => {
-        Dealerships.insertMany(dealerships_data['dealerships']);
+        // Changed ['dealerships'] to .dealerships (Dot Notation)
+        Dealerships.insertMany(dealerships_data.dealerships);
     });
 } catch (error) {
     console.error('Error initializing database documents:', error);
@@ -90,18 +93,19 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     const data = JSON.parse(req.body);
     const documents = await Reviews.find().sort({ id: -1 });
-    let new_id = documents[0]['id'] + 1;
+    // Changed ['id'] to .id (Dot Notation)
+    let new_id = documents[0].id + 1;
 
     const review = new Reviews({
         "id": new_id,
-        "name": data['name'],
-        "dealership": data['dealership'],
-        "review": data['review'],
-        "purchase": data['purchase'],
-        "purchase_date": data['purchase_date'],
-        "car_make": data['car_make'],
-        "car_model": data['car_model'],
-        "car_year": data['car_year'],
+        "name": data.name,
+        "dealership": data.dealership,
+        "review": data.review,
+        "purchase": data.purchase,
+        "purchase_date": data.purchase_date,
+        "car_make": data.car_make,
+        "car_model": data.car_model,
+        "car_year": data.car_year,
     });
 
     try {
